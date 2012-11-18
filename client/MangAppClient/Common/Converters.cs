d8eb56380be59e633effd154eb0ac746c195e927
quimbs <joaquin.bonet@gmail.com>
2012-11-17 18:22:21 -0800

    Subiendo tó pa que se calle er migué

---
+++ client/MangAppClient/Common/Converters.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangAppClient.Common
+{
+    public class LastChapterConverter : Windows.UI.Xaml.Data.IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (targetType == typeof(string))
+            {
+                return String.Format("Last chapter: {0}", value); 
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
